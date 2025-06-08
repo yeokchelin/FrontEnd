425fcaa16e5f1e4ccd3fce574715fc4cf50d71ff
Kanghyeok Jie <kanghyeokjie@gmail.com>
2025-06-08 19:40:31 +0900

    Removed: 더미 상점 데이터
    - 더 이상 사용되지 않는 더미 상점 데이터를 삭제하였습니다

--- src/components/storemanagement/dummyStoreData.jsx
+++
@@ -1,8 +0,0 @@
-// 예시: src/components/storemanagement/dummyStoreData.js
-export const initialStoreInfo = {
-  storeName: '햇살 가득한 빵집',
-  address: '서울시 행복구 기쁨동 123-45',
-  businessHours: '월-금: 09:00 - 20:00\n토: 10:00 - 18:00\n일: 휴무', // 여러 줄 입력 가능하게
-  contactNumber: '02-1234-5678',
-  storeDescription: '매일 아침 직접 구운 신선한 빵과 향긋한 커피를 즐길 수 있는 동네 빵집입니다. 유기농 재료를 사용하여 건강까지 생각합니다.',
-};
